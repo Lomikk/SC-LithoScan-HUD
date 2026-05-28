3c6290b8989ed4c3cade5798635c55ee776febc8
Lomikk <92258604+Lomikk@users.noreply.github.com>
2026-05-28 15:54:04 +0500

    Функционал отладки

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 echo ==================================================
 echo   INITIALIZING STAR CITIZEN OVERLAY SETUP...
 echo ==================================================
